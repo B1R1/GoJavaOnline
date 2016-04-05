ea0a80ea0e7a0d3e4c5e4fbdcf69018fe22f3e88
B1R1 <roman.burko@gmail.com>
2016-04-05 23:49:31 +0300

    Обновил Модуль10, добавил проверку файла на null в новом отдельном классе

--- src/module10/Main.java
+++ src/module10/Main.java
@@ -12,35 +12,30 @@ import java.util.Scanner;
  */
 public class Main {
 
-    private static final String FILE_NAME = "c:\\src\\530.txt";
+    private static final String FILE_NAME = "c:\\535.txt";
 
-    public static void main(String[] args) {
-        File file = new File (FILE_NAME);
-        try {
-            if (!file.exists()) {
-                    if (!file.createNewFile()) {
-                        System.out.println("File already exist");
-                    }
-            }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+    public static void main(String[] args) throws IOException {
 
+        File file = FileChecker.getFile(FILE_NAME);
+
+        try (BufferedWriter writer =
+                     new BufferedWriter(new FileWriter(file)))
+        {
             CaesarCipher cipher = new CaesarCipher();
 
             String message = scanMessage();
 
-            if (!message.isEmpty()) {
+            if (!message.trim().isEmpty()) {
                 String encodedMessage = cipher.encode(message);
                 writer.write("Encoded message is: " + encodedMessage + "\r\n");
 
                 String decodedMessage = cipher.decode(encodedMessage);
-                writer.write("Decoded message is: " + decodedMessage+ "\r\n");
+                writer.write("Decoded message is: " + decodedMessage + "\r\n");
             }
             else System.out.println("Message was empty.");
-
-            writer.close();
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch (FileNotFoundException e) {
+            System.out.println("Can't find file to write");
         }
     }
 
