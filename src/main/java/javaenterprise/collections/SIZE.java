3031b93ea4b94f6fa93fd94dd642f79b5f7e851a
B1R1 <roman.burko@gmail.com>
2016-05-29 19:41:46 +0300

    commit Size class

---
+++ src/main/java/javaenterprise/collections/SIZE.java
@@ -0,0 +1,8 @@
+package javaenterprise.collections;
+
+public class Size {
+    public static final int SIZE_10K = 10_000;
+    public static final int SIZE_100K = 100_000;
+    public static final int SIZE_1000K = 1000_000;
+}
+
